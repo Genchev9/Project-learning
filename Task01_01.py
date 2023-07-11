2b8c1483d0b0d08d61754467dce86c316b1f1fef
Genchev <Genchev9288@gmail.com>
2023-07-11 09:35:49 +0200

    123

---
+++ Task01_01.py
@@ -0,0 +1,5 @@
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+print(my_favorite_songs[0:14],my_favorite_songs[64:77],my_favorite_songs[16:30],my_favorite_songs[51:62])
+print(len(my_favorite_songs))
+my_favorite_songs_1 = ['Waste a Moment', 'Staying Alive','A Sorta Fairytale','Start Me Up','New Salvation']
+print(my_favorite_songs_1[0],my_favorite_songs_1[-1],my_favorite_songs_1[1],my_favorite_songs_1[-2])
